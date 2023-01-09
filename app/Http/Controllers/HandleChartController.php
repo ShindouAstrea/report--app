847b9196af8baa2cec4d52765e093625d044d7e0
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-09 18:00:02 -0300

    change: function to test queries

--- app/Http/Controllers/HandleChartController.php
+++ app/Http/Controllers/HandleChartController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\DB;
+use App\Models\User;
 class HandleChartController extends Controller
 {
-    //
+    public function showCharts(){
+        return('welcome');
+    }
+    // public function handleChart()
+    // {
+    //     $userData = User::select(DB::raw("COUNT(*) as count"))
+    //                 ->whereYear('created_at', date('Y'))
+    //                 ->groupBy(DB::raw("Month(created_at)"))
+    //                 ->pluck('count');
+          
+    //     return view('chartView', compact('userData'));
+    // }
+    public function showRegiones(){
+        $regiones = DB::table('regiones')->get();
+        return view('welcome',compact('regiones'));
+    }
 }
