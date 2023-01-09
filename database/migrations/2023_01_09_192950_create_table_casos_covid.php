69503b137575a55883a1ec9b0be5ed210734c685
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-09 16:39:51 -0300

    Change: refactor in migration function

---
+++ database/migrations/2023_01_09_192950_create_table_casos_covid.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('casos_covid', function (Blueprint $table) {
+            $table->foreignId('idRegion')->constrained('regiones');
+            $table->integer('numeroDeFallecidos');
+            $table->integer('numeroDeCasosAsintomaticos');
+            $table->integer('numeroDeCasosMasculinos');
+            $table->integer('numeroDeCasosFemeninos');
+            $table->string('mesDelReporte');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('casos_covid');
+    }
+};
