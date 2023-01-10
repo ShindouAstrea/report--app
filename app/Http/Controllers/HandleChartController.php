da51afaecd1061d6c9c9b91b5025170eabfebc91
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-10 18:24:46 -0300

    add: functions to get filter data

--- app/Http/Controllers/HandleChartController.php
+++ app/Http/Controllers/HandleChartController.php
@@ -19,8 +19,25 @@ class HandleChartController extends Controller
           
     //     return view('chartView', compact('userData'));
     // }
-    public function showRegiones(){
-        $regiones = DB::table('regiones')->get();
+    public function getRegiones(){
+        $data = DB::table('regiones')->get();
         return view('welcome',compact('regiones'));
+    }
+    public function getCasosCovidByMonth(){
+
+    }
+    public function getAllCasosCovid(){
+        $data = DB::table('regiones')
+                ->select('regiones.nombreRegion','casos_covid.numeroDeCasosAsintomaticos','casos_covid.numeroDeFallecidos','casos_covid.mesDelReporte')
+                ->join('casos_covid','regiones.idRegion','=','casos_covid.idRegion')
+                ->groupBy('casos_covid.mesDelReporte','casos_covid.idRegion','regiones.nombreRegion','casos_covid.numeroDeCasosAsintomaticos','casos_covid.numeroDeFallecidos','casos_covid.mesDelReporte')
+                ->orderBY('casos_covid.mesDelReporte','asc')
+                ->orderBy('casos_covid.idRegion','asc')
+                ->having('casos_covid.mesDelReporte','=','Marzo')
+                ->get();
+                return view('welcome',compact('data'));
+    }
+    public function setTable(){
+        return view('tabla');
     }
 }
