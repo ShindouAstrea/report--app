3b772dc421db1a0701101e9154c15d91ee7d1ebe
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-12 18:14:45 -0300

    FIX: query to sum data

--- app/Http/Controllers/HandleChartController.php
+++ app/Http/Controllers/HandleChartController.php
@@ -16,9 +16,15 @@ class HandleChartController extends Controller
                 ->orderBY('casos_covid.mesDelReporte','asc')
                 ->orderBy('casos_covid.idRegion','asc')
                 ->get();
+        $totalDataBD = DB::table('regiones')
+        ->select('regiones.nombreRegion',DB::raw('SUM(casos_covid.numeroDeCasosAsintomaticos) as numeroDeCasosAsintomaticos'),DB::RAW('SUM(casos_covid.numeroDeFallecidos) as numeroDeFallecidos'),DB::RAW('SUM(casos_covid.numeroDeCasosMasculinos) as numeroDeCasosMasculinos'),DB::RAW('SUM(casos_covid.numeroDeCasosFemeninos) as numeroDeCasosFemeninos'))
+        ->join('casos_covid','regiones.idRegion','=','casos_covid.idRegion')
+        ->groupBy('casos_covid.idRegion','regiones.nombreRegion')
+        ->orderBy('casos_covid.idRegion','asc')
+        ->get();
         //Arreglo que almacenara con clave las regiones con sus respectivos reportes de acuerdo al mes ;
         $data=[];
-        foreach($dataBD as $value){
+        foreach($totalDataBD as $value){
             //Se almacenan todos los reportes de la region actualmente del reporte segun la fecha del reporte
             $casosReportados = [
                 "numeroDeCasosAsintomaticos"=>$value->numeroDeCasosAsintomaticos,
@@ -26,10 +32,9 @@ class HandleChartController extends Controller
                 "numeroDeCasosMasculinos"=>$value->numeroDeCasosMasculinos ,
                 "numeroDeCasosFemeninos"=>$value->numeroDeCasosFemeninos 
             ];
-            $data[$value->nombreRegion][$value->mesDelReporte] = $casosReportados ;
+            $data[$value->nombreRegion] = $casosReportados ;
         };
         $input = $request->all();
         return view('chartView',compact('data'));
     }
-    
 }
