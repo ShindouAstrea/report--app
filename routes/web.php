4d8e7dd901b73a361bd704deb5fa8243da45fb0e
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-12 17:43:51 -0300

    Change: in path route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HandleChartController ;
+use Illuminate\Http\Middleware\HandleCors;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,5 @@ use App\Http\Controllers\HandleChartController ;
 // Initial Route
 
 Route::get('/', [HandleChartController::class, 'getAllCasosCovid']);
+
 Route::get('/table',[HandleChartController::class, 'setTable']);
