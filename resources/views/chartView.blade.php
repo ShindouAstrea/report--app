87b602fe3eed3ad0b70a58a18be2fafd78910c5c
LAPTOP-E4AT5CF7\liset <jose.silva.a@mail.pucv.cl>
2023-01-12 19:31:37 -0300

    Fix: display chart all data

--- resources/views/chartView.blade.php
+++ resources/views/chartView.blade.php
@@ -62,84 +62,64 @@
             <div id="container"></div>
             <script src="https://code.highcharts.com/highcharts.js"></script>
             <script type="text/javascript">
-                Highcharts.chart('container', {
-
-                title: {
-                    text: 'U.S Solar Employment Growth by Job Category, 2010-2020',
-                    align: 'left'
-                },
-
-                subtitle: {
-                    text: 'Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>',
-                    align: 'left'
-                },
-
-                yAxis: {
-                    title: {
-                        text: 'Number of Employees'
-                    }
-                },
-
-                xAxis: {
-                    accessibility: {
-                        rangeDescription: 'Range: 2010 to 2020'
-                    }
-                },
-
-                legend: {
-                    layout: 'vertical',
-                    align: 'right',
-                    verticalAlign: 'middle'
-                },
-
-                plotOptions: {
-                    series: {
-                        label: {
-                            connectorAllowed: false
-                        },
-                        pointStart: 2010
-                    }
-                },
-
-                series: [{
-                    name: 'Installation & Developers',
-                    data: [43934, 48656, 65165, 81827, 112143, 142383,
-                        171533, 165174, 155157, 161454, 154610]
-                }, {
-                    name: 'Manufacturing',
-                    data: [24916, 37941, 29742, 29851, 32490, 30282,
-                        38121, 36885, 33726, 34243, 31050]
-                }, {
-                    name: 'Sales & Distribution',
-                    data: [11744, 30000, 16005, 19771, 20185, 24377,
-                        32147, 30912, 29243, 29213, 25663]
-                }, {
-                    name: 'Operations & Maintenance',
-                    data: [null, null, null, null, null, null, null,
-                        null, 11164, 11218, 10077]
-                }, {
-                    name: 'Other',
-                    data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
-                        17300, 13053, 11906, 10073]
-                }],
-
-                responsive: {
-                    rules: [{
-                        condition: {
-                            maxWidth: 500
-                        },
-                        chartOptions: {
-                            legend: {
-                                layout: 'horizontal',
-                                align: 'center',
-                                verticalAlign: 'bottom'
-                            }
-                        }
-                    }]
+                var datos = <?php echo json_encode($data)?> ;
+                var regiones=Object.keys(datos);
+                var arrayFallecidos = [];
+                var arrayAsintomaticos = [];
+                var arrayMasculinos = [];
+                var arrayFemeninos = [];
+                for( region of regiones){
+                    
+                    arrayFallecidos.push(parseInt(datos[region]["numeroDeFallecidos"]));
+                    arrayAsintomaticos.push(parseInt(datos[region]["numeroDeCasosAsintomaticos"]));
+                    arrayMasculinos.push((datos[region]["numeroDeCasosMasculinos"]));
+                    arrayFemeninos.push(parseInt(datos[region]["numeroDeCasosFemeninos"]));
                 }
+                Highcharts.chart('container', {
+                chart: {
+                    type: 'column',
+                    
+                },
+                title: {
+                    text: 'Reportes de Casos covid por region'
+                },
+                
+                xAxis: {
+                    categories: regiones,
+                    
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: 'Cantidad de personas'
+                    },
+                    
+                },
+               
+                plotOptions: {
+                    column: {
+                        pointPadding: 0,
+                        borderWidth: 0
+                    }
+                },
+                series: [{
+                    name: 'Total Fallecidos',
+                    data: arrayFallecidos
 
-                });
+                }, {
+                    name: 'Total Casos Asintomaticos',
+                    data: arrayAsintomaticos
 
+                }, {
+                    name: 'Total Casos Femeninos',
+                    data: arrayFemeninos
+
+                }, {
+                    name: 'Total Casos Masculinos',
+                    data: arrayMasculinos
+
+                }]
+            });
             </script>
             
         </div>
