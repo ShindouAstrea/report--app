258397ffa1d4ccc2faa47c6941a577120ab16bc5
LAPTOP-APB1FLL2\Jose Antonio <jose.silva.a@mail.pucv.cl>
2023-01-13 04:26:19 -0300

    change: include seeder region and casocovid

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Region;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +15,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+       $this->call(RegionSeeder::class);
+       $this->call(CasoCovidSeeder::class);
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
